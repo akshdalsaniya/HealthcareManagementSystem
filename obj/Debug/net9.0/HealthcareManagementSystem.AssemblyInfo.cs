3b61885c6f3a0856c0d55c862aca64196221d9de
Aksh Dalsaniya <akshdalsaniya0711@gmail.com>
2026-03-02 22:39:24 +0530

    ...

--- obj/Debug/net9.0/HealthcareManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/HealthcareManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthcareManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcafd11411aece68fa47087e14b380b6a6291d9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+724d2c36a99e57e21312cc00fe47567a8fa9be29")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthcareManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthcareManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
